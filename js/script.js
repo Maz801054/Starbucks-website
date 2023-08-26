aa666e71a019f6d057037502dadcabd8f4b026fe
Maz801054 <onemake125@gmail.com>
2023-08-26 11:08:54 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function firstImg() {
+  document.getElementById("glas1").src = "../images/img-1.png";
+  document.getElementById("round1").style.backgroundColor = " #017143";
+  document.getElementById("starChange").style.color = "#017143";
+  document.getElementById("btn1").style.color = "white";
+  document.getElementById("btn1").style.backgroundColor = " #017143";
+  // document.getElementById("logoChange").src = "../images/logo.png";
+}
+
+function secondImg() {
+  document.getElementById("glas1").src = "../images/img-2.png";
+  document.getElementById("round1").style.backgroundColor = " #eb7895";
+  document.getElementById("starChange").style.color = "#eb7895";
+  document.getElementById("btn1").style.color = "white";
+  document.getElementById("btn1").style.backgroundColor = " #eb7895";
+  // document.getElementById("logoChange").src = "../images/pink-logo.png";
+}
+
+function therdImg() {
+  document.getElementById("glas1").src = "../images/img-3.png";
+  document.getElementById("round1").style.backgroundColor = " #d752b1";
+  document.getElementById("starChange").style.color = "#d752b1";
+  document.getElementById("btn1").style.color = "white";
+  document.getElementById("btn1").style.backgroundColor = " #d752b1";
+  // document.getElementById("logoChange").src = "../images/darkpink-logo.png";
+}
